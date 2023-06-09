c46d1f50658e441922bd9db613be35247bf03a04
mohi <mohisadeghi302@gmail.com>
2023-06-09 21:35:40 +0330

    Add open etter

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  config.assets.debug = true
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
